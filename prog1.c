83872269b4adc417e3aefeebd69704e32dd06669
PavanSabani <shilpabhoosaraddi5@gmail.com>
2024-09-23 14:20:48 +0530

    created prog1 file.

--- prog1.c
+++ prog1.c
@@ -1,6 +1,16 @@
-#include<stdio.h>
-int main()
+X#include<stdio.h>
+void main()
 {
-printf("Welcome to Github");
+printf("Study tonight - Best place to learn C");
+int n,i;
+printf("\nEnter an integer you to  need to print the table of: ");
+scanf("%d",&n);
+printf("\n");
+for(i=1;i<=10;i++)
+{
+printf("%d * %d = %d\n",n,i,n*i);
+}
+printf("\nCoding is Fun\n");
+
 }
 
