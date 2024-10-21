70bf9c38dd14faf978e899634fd5d75d036f909b
AKSHAYJCHIKKERUR <akshay.chikerur@gmail.com>
2024-10-21 15:03:05 +0530

    this line i m adding in my localrepo on monday

--- 1.c
+++ 1.c
@@ -3,5 +3,6 @@ int main()
 {
 printf("changes done for showing stash command"):
 printf("nothing change");
-printf("this line i am adding in my local repo on monday")
 }
+this line i am adding in my local repo on monday
+
